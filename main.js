1de86ac5541768aaa0782071f96d3e5b074904d6
ramiro acosta <ramiroacostal153@gmail.com>
2023-01-26 15:52:44 -0300

    lo volvi a hacer

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-class ProductManager {
+class product {
     constructor(title, description, price, thumbnail, code, stock) {
         this.title = title
         this.description = description
@@ -6,22 +6,64 @@ class ProductManager {
         this.thumbnail = thumbnail
         this.code = code
         this.stock = stock
-        this.getProducts =[]
-        }
-        addPruduct(){
-            const product1 = {tittle:"iphone11", description:"celular nuevo", price:1000, thumbnail:"sin imagen", code:"ad684",stock: 10 }
-            this.getProducts.push(product1)
-            console.log(code.includes("ad684"))
-            function incrementador(id=1){
-                while(true){
-                    yield id;
-                    ++id;
-                }
-            }
-            let generador = incrementador()
-            console.log(generador.next())
-        }
-        get getProducts(){
-            return this.getProducts
-        }
+    }
 }
+class ProductManager {
+    constructor() {
+        this.product = []
+    }
+    addProduct(newProduct) {
+        if (toString(newProduct.id).length>0 && newProduct.title.length>0 && newProduct.description.length>0 && toString(newProduct.price).length>0 && newProduct.thumbnail.length>0 && newProduct.code.length>0 && toString(newProduct.stock).length>0){
+            if(this.product.filter(product=> product.code==newProduct.code).length > 0){
+                console.log("El producto ya existe")
+            }else{
+                const id = ProductManager.idauto()
+                this.product.push({id: id, ...newProduct})
+            }
+        }else{
+            console.log("Para agregar el producto debe cumplir con todos los requisitos")
+        }
+    }
+    getProducts() {
+        return this.product;
+    }
+    getProductsById(id){
+        if(this.product.find(product => product.id == id)!=undefined){
+            return this.product.find(product => product.id == id)
+        }else{
+            return "no existe el producto"
+        }
+    }
+    //clase stactic para el id automatico
+    static idauto() {
+        if(!this.idAnterior) {
+            this.idAnterior = 1
+        }else{
+            this.idAnterior++
+        }
+        return this.idAnterior
+    }
+}
+//creo todos los productos(van sin imagen)
+const producto1=new product("Iphone 14","celular nuevo",1500,"sin imagen","#ghy123",30);
+const producto2=new product("Iphone 13","celular nuevo",1400,"sin imagen","#ghy128",25);
+const producto3=new product("Iphone 12","celular nuevo",1300,"sin imagen","#ghy126",17);
+const producto4=new product("Iphone 11 pro","celular nuevo",1200,"sin imagen","#ghy124",14);
+const producto5=new product("Iphone 11","celular nuevo",1000,"sin imagen","#ghy127",10);
+
+productMaganer=new ProductManager()
+
+//muestro array vacio
+console.log(productMaganer.getProducts())
+
+//Agrego los productos al product manager
+productMaganer.addProduct(producto1);
+productMaganer.addProduct(producto2);
+productMaganer.addProduct(producto3);
+productMaganer.addProduct(producto4);
+productMaganer.addProduct(producto5);
+
+//muestro los productos
+console.log(productMaganer.getProducts())
+//muestro producto by id
+console.log(productMaganer.getProductsById(3))
